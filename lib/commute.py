2c7c272269a769d6e332e7012b9516f5c03d182a
Volkan Unsal <spocksplanet@gmail.com>
2014-04-01 16:17:03 -0400

    Ensure the script can open the data files when called from another directory

--- lib/commute.py
+++ lib/commute.py
@@ -1,11 +1,12 @@
 
 import networkx as nx
 import pickle
+import os
 
 # TODO: write documentation
 def commute_search(stop, time):
     time = float(time)
-    subway = pickle.load(open('subway_network','r'))  # networkx graph with subway stops and times        
+    subway = pickle.load(open('subway_network','r'))  # networkx graph with subway stops and times
     schools = pickle.load(open('school_stops'))  # dictionary with list of subway stop for a school
     potential_schools = []
     for school in schools:
@@ -30,21 +31,21 @@ def subway_time(subway, stop1, stop2):
         time+=subway[previous][stop]['time']
         previous = stop
     return time/60.0
-       
+
 
 #=========SCRIPT STARTS HERE===============#
 if __name__ == '__main__':
     import sys
     if len(sys.argv)==2:
         if sys.argv[1] == 'stops':
-            subway = pickle.load(open('subway_network','r'))  # networkx graph with subway stops and times        
+            subway = pickle.load(open(os.path.dirname(os.path.realpath(__file__))+'/subway_network','r'))  # networkx graph with subway stops and times
             print(subway.nodes())
         elif sys.argv[1] == 'schools':
-            schools = pickle.load(open('school_stops'))  # dictionary with list of subway stop for a school
+            schools = pickle.load(open(os.path.dirname(os.path.realpath(__file__))+'/school_stops'))  # dictionary with list of subway stop for a school
             print(schools.keys())
         else:
             print('Unknown parameter options supplied:',sys.argv,'Try "stops" or "schools"')
-    elif len(sys.argv) == 3:        
+    elif len(sys.argv) == 3:
         try:
             print(commute_search(sys.argv[1], sys.argv[2]))
         except:
