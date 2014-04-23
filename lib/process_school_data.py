f01396928874a5bcc21039836730988fc85f6687
seanluciotolentino <seanluciotolentino@gmail.com>
2014-04-23 12:58:10 -0400

    Manual Updates to school's stops

--- lib/process_school_data.py
+++ lib/process_school_data.py
@@ -24,7 +24,7 @@ for l in f:
     names[stop_name] = stop_id
 
 #go through schools and pick out subway stops
-schools = {}  # {school_name : [stop1, stop2, ...]}
+stops_for = {}  # {school_name : [stop1, stop2, ...]}
 
 #http://nycdoe.pediacities.com
 f = open('doe_data/schools20131023.csv','r')
@@ -37,13 +37,60 @@ for l in f:
     buses = line[2]
     subway_stops = [stop.split(" to ")[-1] for stop in line[3].split(";")]
     
-    schools[school_name] = []
+    stops_for[school_name] = []
     for s in subway_stops:        
         try:
-            schools[school_name].append(names[s])
+            stops_for[school_name].append(names[s])
         except KeyError:
-            print "stop",s,"school",school_name,"other stops",schools[school_name]
+            print "stop",s,"school",school_name,"other stops",stops_for[school_name]
 
-print "match rate:",sum([len(schools[school_name])>0 for s in schools])/float(len(schools))
+print "match rate:",sum([len(stops_for[s])>0 for s in stops_for])/float(len(stops_for))
 
-pickle.dump(schools,open('school_stops','w'))
+#pickle.dump(stops_for,open('school_stops','w'))
+"""
+THINGS I HAD TO MANUALLY DO:
+th --> ''
+Port Auority --> Port Authority  *
+Winrop --> Winthrop
+Norern --> Northern
+Central Park Nor (110 St) --> Central Park North (110 St)
+
+rd --> ''
+Bedfo --> Bedford
+Gaen --> Garden
+Foham Road --> Fordham Rd
+
+Square --> Sq
+Street --> St
+Bouleva --> Blvd
+
+EastBroadway --> East Broadway
+1stAve --> 1 Av
+1StAv --> 1 Av
+Astor Place --> Astor Pl
+Whitehall St - Sou Ferry--> Whitehall
+Broadway - Lafayette St-->Broadway-Lafayette St
+Van Wyck Blvd --> Briarwood - Van Wyck Blvd
+Atlantic Av - Barclays Center - 4 Av --> Atlantic Av - Barclays Ctr
+Sutphin Blvd -  Archer Avenue --> Sutphin Blvd - Archer Av - JFK Airport
+Broadway Junction --> Broadway Jct
+ForeStAve --> Forest Av
+Neptune Av - Van Siclen --> Neptune Av
+WeSt8 St --> W 8 St - NY Aquarium
+Union Turnpike - Kew Gardens --> Kew Gardens - Union Tpke
+Kings Highway --> Kings Hwy
+Beach 105 St - Seaside --> Beach 105 St
+179 St --> Jamaica - 179 St
+
+
+disambiguation needed:
+Bay Pkwy
+Bedford Park
+Broadway Jct
+
+High School for Construction Trades
+Stephen T. Mather Building Arts & Craftsmanship High School
+High School for Law Enforcement and Public Safety
+
+
+"""
